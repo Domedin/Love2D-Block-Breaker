5cc8b6745cff7650e61b65c8596f95acb284ad44
Domedin <jackanderson080307@gmail.com>
2023-09-12 20:52:32 -0400

    Working on getting map working

--- blocks.lua
+++ blocks.lua
@@ -1,14 +1,13 @@
 Blocks = {}
 
 function Blocks:load()
-    Blocks.blockTypeImg = {"sprites/GreenBlock.png", "sprites/LightGreenBlock.png", "sprites/YellowBlock.png", "sprites/OrangeBlock.png", "sprites/RedBlock.png"}
-    Blocks.blockTypeValue = {5, 4, 3, 2, 1}
+    sti = require 'Libraries/Simple-Tiled-Implementation/sti'
 end
 
 function Blocks:update(dt)
     
 end
 
-function Blocks:draw()  
+function Blocks:draw()
     
 end
